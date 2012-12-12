f718aed168007382d2526e04f02e21845597f8a5
Dasharath <dxg125030@utdallas.edu>
2012-12-12 12:12:08 -0600

    adding JobSeeker tables

---
+++ App_data/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[JobSeeker]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstName] NCHAR(20) NULL, 
+    [middleName] NCHAR(20) NULL, 
+    [lastName] NCHAR(10) NULL, 
+    [userName] NCHAR(10) NULL, 
+    [email] NCHAR(10) NULL, 
+    [currentAddress] NCHAR(30) NULL, 
+    [currentState] NCHAR(10) NULL, 
+    [zipCode] NCHAR(10) NULL, 
+    [phone] NCHAR(10) NULL, 
+    [nationality] NCHAR(10) NULL
+)
