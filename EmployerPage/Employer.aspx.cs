96e386d741fa14f16cf443dddea67148a6f50fd5
Dasharath <dxg125030@utdallas.edu>
2012-12-14 11:40:58 -0600

    login check for employer

--- EmployerPage/Employer.aspx.cs
+++ EmployerPage/Employer.aspx.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Web.Security;
 public partial class Employer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string loggedinuser = null;
+        MembershipUser membershipUser = Membership.GetUser();
+        if (membershipUser != null)
+        {
+            loggedinuser = Membership.GetUser().ToString();
+        }
+
+        if (loggedinuser == null || "".Equals(loggedinuser))
+        {
+            Response.Redirect("../Login.aspx");
+        }
 
     }
     protected void Button2_Click(object sender, EventArgs e)
